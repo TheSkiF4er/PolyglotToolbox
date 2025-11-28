c5462578452c2773606b5416ea25e0e846d285f1
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-28 19:19:45 +0300

    Update build.gradle.kts

--- languages/kotlin/build.gradle.kts
+++ languages/kotlin/build.gradle.kts
@@ -1,1 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.8.22"
+    application
+}
 
+group = "com.polyglot.toolbox"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+application {
+    mainClass.set("com.polyglot.toolbox.TextStatsKt")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
